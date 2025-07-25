6ea1a4c0853f2d66c91a12174650c1882a5aff0c
nnsncl <nuniroland@gmail.com>
2025-07-25 19:03:54 +0200

    Update transformers: add compound components declarations

--- src/core/file-operations.ts
+++ src/core/file-operations.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
-import { success, failure, tryCatch } from "../utils/result";
+import { failure, tryCatch } from "../utils/result";
 
 import type { SvgFile, Result } from "../types";
 
